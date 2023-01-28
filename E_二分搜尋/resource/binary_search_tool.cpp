d897aaab172099a083a7492901a554eb4eebcac7
temmie_950807 <kapoo950807@gmail.com>
2023-01-28 10:08:02 +0800

    add A

--- E_二分搜尋/resource/binary_search_tool.cpp
+++
@@ -1,7 +0,0 @@
-// 使用前請記得sort!!!
-// 尋找是否有val，回傳true或false
-cout << binary_search(v.begin(), v.end(), val) << endl;
-
-// 尋找大於等於/大於val的第一個值，回傳此值的iterator
-lower_bound(v.begin(), v.end(), val);
-upper_bound(v.begin(), v.end(), val);

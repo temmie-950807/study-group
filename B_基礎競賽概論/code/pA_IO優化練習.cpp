13865d7d49bca91ff3e2d163a21ff7460f035526
temmie_950807 <kapoo950807@gmail.com>
2023-01-30 12:06:37 +0800

    修改「IO優化部份敘述」、修改些許細節

---
+++ B_基礎競賽概論/code/pA_IO優化練習.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// declare
+int n;
+string s;
+
+int main(){
+    // fast IO
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // input
+    cin >> n;
+    for (int i=0 ; i<n ; i++){
+        cin >> s;
+        cout << s << " eat pasta pls uwu\n";
+    }
+    return 0;
+}
